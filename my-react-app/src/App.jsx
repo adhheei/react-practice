9f1c93585988cf69f2ff720286953c25b80f8942
adithyaadhiii <adithyamv06@gmail.com>
2026-05-03 12:12:05 +0530

    initial commit

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,61 @@
+// function App() {
+//   return (
+//     <div>
+//     <h1>My name is Adithya</h1>
+//     <p>Favorite Color : Blue</p>
+//     <p>Favorite Food : Biriyani</p>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+// import "./App.css"
+
+// function App(){
+//   const name ="Adhi";
+//   const age =22;
+
+//   return(
+//     <div className="card">
+//       <h1>{name}</h1>
+//       <p>Age:{age}</p>
+//       <p>I am learing React</p>
+//     </div>
+//   )
+// }
+
+// export default App;
+
+// import React from 'react'
+// import User from './User'
+
+// function App() {
+//   return (
+//     <div>
+//       <User name="Adhi" age={25} city="malappuram"/>
+//       <User name="abhi" age={28} city="chalavara"/>
+//       <User name="sachu" age={17} city="calicut"/>
+//     </div>
+//   )
+// }
+
+// export default App
+
+import React, { useState } from 'react'
+
+function App() {
+
+  const [count,setcount] = useState(0);
+
+  return (
+    <div>
+      <h1>Count:{count}</h1>
+      <button onClick={()=>setcount(count+1)}>increase</button>
+      <button onClick={()=>setcount(count-1)}>decrease</button>
+    </div>
+  )
+}
+
+
+export default App
---
+++ my-react-app/src/User.jsx
@@ -0,0 +1,14 @@
+// import React from 'react'
+// import './App.css'
+
+// function User({name,age,city}) {
+//   return (
+//     <div className='card'>
+//         <h2>Name:{name}</h2>
+//         <p>Age:{age}</p>
+//         <p>City:{cit}</p>
+//     </div>
+//   );
+// }
+
+// export default User
